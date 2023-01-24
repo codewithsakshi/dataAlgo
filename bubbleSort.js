7907730718a17022c44b3cf9fac8fc312338c95d
codewithsakshi <sakshimittal3005@gmail.com>
2023-01-24 15:15:23 +0530

    first commit

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,3 +1,44 @@
+//   What is bubble sort?
+// 1. It includes more sort.
+// 2. It is slow in comparison of Insertion sort algorihtm.
+// 3. compares two items in an array that are next to each other and swap them if they are out of order(that is if larger one comes first in the array) then 
+//  move forward one index, compares again and swap again. once we run through the whole array with no swap array is sorted.
+
+
+
+// [1, 5, 4, 2, 3]
+
+// Are 1 and 5 out of order? No.
+// Are 5 and 4 out of order? Yes. Swap.
+
+// [1, 4, 5, 2, 3]
+
+// Are 5 and 2 out of order? Yes. Swap.
+
+// [1, 4, 2, 5, 3]
+
+// Are 5 and 3 out of order? Yes. Swap.
+
+// [1, 4, 2, 3, 5]
+
+// End of the array, did we swap anything? Yes. Loop again.
+// Are 1 and 4 out of order? No.
+// Are 4 and 2 out of order? Yes. Swap.
+
+// [1, 2, 4, 3, 5]
+
+// Are 4 and 3 out of order? Yes. Swap.
+
+// [1, 2, 3, 4, 5]
+
+// Are 4 and 5 out of order? No.
+// End of the array, did we swap anything? Yes. Loop again.
+// Are 1 and 2 out of order? No.
+// Are 2 and 3 out of order? No.
+// Are 3 and 4 out of order? No.
+// Are 4 and 5 out of order? No.
+// End of the array, did we swap anything? No. List is sorted.
+
 function bubbele(nums) {
 
     let isSwap = false;
--- insertionSort.js
+++ insertionSort.js
@@ -1,3 +1,47 @@
+// What is insertion sort
+// In the insertion sort we transfer an element at one time to construct sorted array.
+// Insertion sort we used for that type of data that we know is almost sorted only some items we need to do transfer to sort the array.
+
+// Mechanism of Insertion sort =>
+// The mechanism we follow is we keep moving bigger part to the right by swapping items in the array as we move acros the element.
+//  When we come to the place we are going to insert, we'll stop doing those swaps.  
+
+
+// [3, 2, 5, 4, 1]
+
+//     ↓
+// [3, 2, 5, 4, 1]  the ↓ is the number we're looking to insert, everything before is sorted
+
+// Is 2 larger than 3? No. Move 3 to the right.
+// Beginning of list, insert 2 at index 0.
+
+//        ↓
+// [2, 3, 5, 4, 1]
+
+// Is 5 larger than 3? Yes.
+// Insert after 3 (it's already there so it doesn't move)
+
+//           ↓
+// [2, 3, 5, 4, 1]
+
+// Is 4 larger than 5? No. Move 5 to the right.
+// Is 4 larger than 3? Yes.
+// Insert after 3 at index 2.
+
+//              ↓
+// [2, 3, 4, 5, 1]
+
+// Is 1 larger than 5? No. Move 5 to the right.
+// Is 1 larger than 4? No. Move 4 to the right.
+// Is 1 larger than 3? No. Move 3 to the right.
+// Is 1 larger than 2? No. Move 2 to the right.
+// Beginning of list, insert 1 at index 0
+
+// [1, 2, 3, 4, 5]
+
+// Reached end of list, list is sorted.
+
+
 function insertion(nums) {
 
     for(let i = 1; i < nums.length; i++){
